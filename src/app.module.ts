8dd7900eaa530cfd76fd995b9eeb93cf551e4d0c
theoxav <theoxxav@outlook.fr>
2024-11-02 18:06:26 +0100

    Add subject module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SubjectController } from './subject/subject.controller';
-import { SubjectService } from './subject/subject.service';
+import { SubjectModule } from './subject/subject.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, SubjectController],
-  providers: [AppService, SubjectService],
+  imports: [SubjectModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
